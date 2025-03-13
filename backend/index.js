181abb1296b539a6c7c9e7363a53613d02f10f9d
ChakradharA2003 <chakradharchakri018@gmail.com>
2025-03-13 14:20:19 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -7,4 +11,15 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => { console.log(err); });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
